ef026cfc53ac2aa9c7058046247e8e8bf16f1d91
Kevin Iglesias <kiglesias@meetsoci.com>
2026-06-19 11:19:57 +0800

    update

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -1,7 +1,9 @@
 import { UserModel } from "./UserModel";
 import { LocatorModel } from "./LocatorModel";
+import { LocationModel } from "./LocationsModel";
 
 export {
   UserModel,
   LocatorModel,
+  LocationModel,
 }
