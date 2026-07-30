7b00e2dfeec1d0013009cc2b8e8998bca2eed138
Kevin Iglesias <kiglesias@meetsoci.com>
2026-07-30 11:43:26 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-const nextConfig: NextConfig = {
-	async headers() {
-		return [
-			{
-				source: '/:path*',
-				headers: [
-					{
-						key: 'X-Robots-Tag',
-						value: 'noindex, nofollow',
-					},
-				],
-			},
-		];
-	},
-};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
